f22ec684dcb1b9fb6112d877c9ec7c32f6d51e71
Reza <99068159@mydavinci.nl>
2023-03-22 14:26:27 +0100

    Heb de get methode toegepast en daardoor een php file aangemaakt voor de html form

---
+++ welcome.php
@@ -0,0 +1,20 @@
+<?php var_dump($_GET) ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="welcome.css">
+    <title>Result simple form</title>
+</head>
+<body>
+    <div class="container2">
+    <!-- <?php var_dump($_GET) ?> -->
+    <h1>The information entered is:</h1>
+    Name: <?php echo $_GET["name"]; ?><br>
+    Email: <?php echo $_GET["email"] ?> <br>
+    </div>
+</body>
+</html>

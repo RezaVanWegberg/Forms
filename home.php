bc05cc402d542d66fa0f964c5a03b4d30bf704e8
Reza <99068159@mydavinci.nl>
2023-03-31 12:13:03 +0200

    Heb het afgemaakt en heb de nieuwe manier van de les toegepast

--- home.php
+++ home.php
@@ -1,6 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    $error = [];
 
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+
+
+        foreach($_POST as $key => $value){
+            if (!$value) {
+                $error[$key] = "$key is niet ingevuld";
+            }
+        };
+    } else {
+    };
+?>
+
+<!DOCTYPE html>
+<html>
+    
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,60 +24,45 @@
     <title>Document</title>
 </head>
 
-<body>
-<?php
-
-$nameErr = $emailErr = "";
-$name = $email = "";
-
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
-        $nameErr = "Name is required";
-      } else {
-        $name = test_input($_POST["name"]);
-      }
-      
-      if (empty($_POST["email"])) {
-        $emailErr = "Email is required";
-      } else {
-        $email = test_input($_POST["email"]);
-      }
-  }
-  
-  function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-?>
-
-
+    <body>
     <div class="container">
+
+    <?php  if ($_SERVER['REQUEST_METHOD'] == "GET" || !empty($error)) { ?>
+            
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-            <p>
                 <label for="name">Name :</label>
-                <input type="text" name="name" id="name" value="<?php echo $name;?>">
-                <span class="error">* <?php echo $nameErr;?></span>
+                <input type="text" name="name" id="name" value="<?php echo isset($_POST['name']) ? $_POST['name'] : '' ?>">
+                <span style="color:red">*</span>
+                <?php if(array_key_exists("name", $error)){ ?>
+                    <p style="color:red"> je hebt dit veld niet ingevuld </p>
+                <?php } ?>
+
             </p>
             <p>
-                <label for="email">Email :</label>
-                <input type="text" name="email" id="email" value="<?php echo $email;?>">
-                <span class="error">* <?php echo $emailErr;?></span>
+                <label for="Email">Email :</label>
+                <input type="text" name="email" id="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>">
+                <span style="color:red">*</span>
+                <?php if(array_key_exists("email", $error)){ ?>
+                    <p style="color:red"> je hebt dit veld niet ingevuld </p>
+                <?php } ?>
 
             </p>
             <input id="submit" type="submit" value="submit">
         </form>
+
+        <?php }  else { ?>            
         <?php
         echo "<h2>Your Input:</h2>";
-        echo $name;
+        echo "Your name: ";
+        echo $_POST['name'];
         echo "<br>";
-        echo $email;
+        echo "Your email: ";
+        echo $_POST['email'];
+        ?>
 
-?>
-    </div>
-</body>
+            </div>
+        <?php } ?> 
 
+        </div>
+    </body>
 </html>
